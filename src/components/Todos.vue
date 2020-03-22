e6a4c9e52fe656f01ce752aa11a729b694feb0e8
oguzhannsahinn <oguzhansahin95@outlook.com>
2020-03-22 18:12:57 +0300

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2> My Todo List </h2>
+
+        <ul>
+            <li v-bind:key="todo.id" v-for="todo in todos">
+                <Todo v-bind:todo="todo" />
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+
+import Todo from './Todo';
+
+export default {
+    name: 'Todos',
+    components: {
+        // eslint-disable-next-line vue/no-unused-components
+        Todo
+    },
+    props: [
+        "todos"
+    ]
+}
+</script>
+
+<style scoped>
+</style>
